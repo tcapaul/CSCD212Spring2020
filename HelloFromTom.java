85b895f2fb7a24ec74c622cce6cf80f1572abf46
Tom Capaul <tcapaul@ewu.edu>
2020-05-11 16:25:27 -0700

    added HelloFromTom.java to repo

---
+++ HelloFromTom.java
@@ -0,0 +1,9 @@
+public class HelloFromTom{
+	
+	public static void main(String [] args){
+		System.out.println("Hello from TomsBranch");
+		System.out.println("This file was created on Tom's machine");
+	}
+	
+	
+}
